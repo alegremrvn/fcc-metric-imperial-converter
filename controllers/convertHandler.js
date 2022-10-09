5066e13e39b31799362f2bd43bea60cf897d8890
Marvin Alegre <alegremrvn@protonmail.com>
2022-10-09 20:05:38 +0800

    Add .getNum method.

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,8 +1,46 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
+    if (input === '') return 1
+
     let result;
-    
+    let firstLetter = input.match(/[a-zA-z]/)
+  
+    // unvalidatedNum.length !== 0
+    let validateNum = (unvalidatedNum) => {
+      if (unvalidatedNum.match(/[0-9\.\/]/gi).length === unvalidatedNum.length) {
+        if (unvalidatedNum[0] === '/' ||
+        unvalidatedNum[unvalidatedNum.length - 1] === '/') {
+          return "invalid number"
+        }
+  
+        let nums = unvalidatedNum.split('/')
+        if (nums.length <= 2) {
+          for (let num of nums) {
+            if (Number.isNaN(Number(num))) return 'invalid number'
+          }
+  
+          if (nums.length === 2) return Number(nums[0]) / Number(nums[1])
+          return Number(nums[0])
+        } else {
+          return 'invalid number'
+        }
+      } else {
+        return 'invalid number'
+      }
+    }
+  
+    if (firstLetter) {
+      let unvalidatedNum = input.slice(0, firstLetter.index)
+      if (unvalidatedNum.length === 0) {
+        result = 1
+      } else {
+        result = validateNum(unvalidatedNum)
+      }
+    } else {
+      result = validateNum(input)
+    }
+  
     return result;
   };
   
